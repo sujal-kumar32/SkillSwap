b6c33fc276537f2ae655f7aafead8902574f19b9
sujal-kumar32 <sujalstark12345@gmail.com>
2026-05-29 00:27:28 +0530

    production api url

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'https://skillswap-0weh.onrender.com/api',
     },
   },
 })
