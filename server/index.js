2a8c1e68d68287256fec5e34e9658a0dd84427b3
sujal-kumar32 <sujalstark12345@gmail.com>
2026-06-06 02:06:10 +0530

    add Express compression middleware for gzip response encoding

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const helmet = require("helmet");
+const compression = require("compression");
 const rateLimit = require("express-rate-limit");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
@@ -42,6 +43,7 @@ const notificationRoutes = require("./routes/notificationRoutes");
 const analyticsRoutes = require("./routes/analyticsRoutes");
 
 app.use(helmet());
+app.use(compression());
 app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
